888842f00a89c7bae8d5d1afafaa3de698d702b5
leonardoTavaesM <leomaltadesa0712@gmail.com>
2025-09-07 17:16:32 -0300

    feat: implement debouncing mechanism in WatcherService to optimize file event handling

--- internal/service/watcher/watcher.go
+++ internal/service/watcher/watcher.go
@@ -1,6 +1,7 @@
 package watcher
 
 import (
+	"sync"
 	"time"
 
 	"github.com/leonardoTavaresM/watcher/internal/domain"
@@ -8,13 +9,24 @@ import (
 
 type WatcherService struct {
 	publisher domain.EventPublisher
+	lastEvent map[string]time.Time
+	mu        sync.Mutex
+	debounce  time.Duration
 }
 
 func NewWatcherService(pub domain.EventPublisher) *WatcherService {
-	return &WatcherService{publisher: pub}
+	return &WatcherService{
+		publisher: pub,
+		lastEvent: make(map[string]time.Time),
+		debounce:  750 * time.Millisecond,
+	}
 }
 
 func (s *WatcherService) HandleFileEvent(path, ext, evType string) error {
+	if ok := s.ShouldProcess(path); !ok {
+		return nil
+	}
+
 	event := domain.FileEvent{
 		Timestamp: time.Now(),
 		FilePath:  path,
@@ -23,4 +35,20 @@ func (s *WatcherService) HandleFileEvent(path, ext, evType string) error {
 	}
 
 	return s.publisher.Publish(event)
+}
+
+func (s *WatcherService) ShouldProcess(path string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	now := time.Now()
+
+	if last, ok := s.lastEvent[path]; ok {
+		if now.Sub(last) < s.debounce {
+			return false
+		}
+	}
+	s.lastEvent[path] = now
+
+	return true
 }
